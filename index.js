49de5372ac6add25b5e27db343988bc80a434a81
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2022-08-19 17:46:28 +0530

    Update  index.js

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
-const num1 = Math.ceil(Math.random() * 10)
-const num2 = Math.ceil(Math.random() * 10)
 
-const questionEl = document.getElementById("question");
-
-
-const inuptEL = document.getElementById("input");
-
-const formEl = document.getElementById("form");
-
-const scoreEl = document.getElementById("score");
-
-let score = JSON.parse(localStorage.getItem("score"));
-
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+var score = JSON.parse(localStorage.getItem("score"));
 if (!score) {
-    score = 0;
+  score = 0;
 }
+$("#scoreId").html(score)
+$("#num1").html(num1);
+$("#num2").html(num2);
+var correctAns = num1 * num2;
+$("#correct").html(correctAns);
 
-scoreEl.innerText = `score:${score}`
-
-
-
-questionEl.innerText = `what is ${num1} multiply by ${num2} ? `
-
-const correctAns = num1 * num2;
-formEl.addEventListener("submit", () => {
-
-
-    const userAns = +inputEL.value
-    if (userAns === correctAns) {
-        score++
-        updatelocalStorage()
-    }
-    else {
-        score--
-        updatelocalStorage()
-    }
-});
-
+function refreshData(){
+  let num1New = Math.ceil(Math.random() * 10);
+  let num2New = Math.ceil(Math.random() * 10);
+  $("#num1").html(num1New);
+  $("#num2").html(num2New);
+  correctAns = num1New * num2New;
+  $("#correct").html(correctAns);
+}
+$("#submitAns").click(function(event) {
+  event.preventDefault();
+  if($("#input").val() == correctAns){
+    score++;
+    console.log(score)
+    updatelocalStorage();
+  }else{
+    score--;
+    updatelocalStorage();
+  }
+  $("#scoreId").html(score)
+  refreshData();
+})
 function updatelocalStorage() {
     localStorage.setItem("score", JSON.stringify(score))
-}   
+}
